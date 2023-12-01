a3c5aa6107727b3f00f52cbeaa9a3d4615b6f0f8
Harry <harrisonhemstreet@proton.me>
2023-11-30 18:24:23 -0700

    create a script that can function as a CLI tool for asking Ollama models questions with the ask binary

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use spinners_rs::{Spinner, Spinners};
 use std::env;
-use std::io::{self, Write};
-use std::process::{Command, Output};
+use std::process::{Command, Output, Stdio};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -70,20 +73,46 @@ fn install_ollama() -> Result<Output, std::io::Error> {
 }
 
 fn run_ollama(model: &str, question: &str) -> Result<Output, std::io::Error> {
-    let output = Command::new("ollama")
-        .arg("run")
-        .arg(model)
-        .arg(question)
-        .output()?;
+    let loading_msg: String = format!("Running {}...", model);
+    let mut spinner = Spinner::new(Spinners::Aesthetic, loading_msg);
+    spinner.start();
 
-    // Check if the command execution was successful
-    if output.status.success() {
-        // If successful, print stdout and stderr
-        io::stdout().write_all(&output.stdout)?;
-        io::stderr().write_all(&output.stderr)?;
-    } else {
-        // If failed, print an error message
-        eprintln!("Ollama command failed to execute");
+    let output = Arc::new(Mutex::new(None));
+    let output_clone = Arc::clone(&output);
+
+    let model_clone = model.to_string();
+    let question_clone = question.to_string();
+    thread::spawn(move || {
+        let command_output = Command::new("ollama")
+            .arg("run")
+            .arg(&model_clone)
+            .arg(&question_clone)
+            .stdout(Stdio::piped()) // Capture stdout
+            .stderr(Stdio::piped()) // Capture stderr
+            .output();
+
+        let mut output = output_clone.lock().unwrap();
+        *output = Some(command_output);
+    });
+
+    let mut command_output: Option<Output> = None;
+    while command_output.is_none() {
+        let output = output.lock().unwrap();
+        if let Some(result) = &*output {
+            command_output = result.as_ref().ok().cloned();
+            break;
+        }
+        drop(output); // Release the lock before sleeping
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    spinner.stop();
+
+    let output = command_output.unwrap();
+    // Print the captured output
+    println!("\r{}", String::from_utf8_lossy(&output.stdout));
+    if !output.stderr.is_empty() {
+        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
     }
 
     Ok(output)
